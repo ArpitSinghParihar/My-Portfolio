82f2584cb155e4a7d4c13d1bb8915f8e0c590f65
Arpit Singh Parihar <parihararpit455@gmail.com>
2025-06-25 10:57:11 +0530

    Responsive

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -5,12 +5,27 @@ import theme from '../../assets/theme_pattern.svg'
 const Resume = () => {
     return (
         <div id='resume' className='resume'>
-            <div className='resume-title'>
-                <h1>My Resume</h1>
-                <img src={theme} alt="" />
+            <div className='resume-img-section'>
+                <div className='resume-title'>
+                    <h1>My Resume</h1>
+                    <img src={theme} alt="" />
+                </div>
+                <iframe src={resume_img} title="Resume" className="resume-frame"></iframe>
             </div>
-            <img className='resume-img' src={resume_img} alt="" />
+            {/* <img className='resume-img' src={resume_img} alt="" /> */}
+            <div className="resume-button">
+                <a
+                    href={resume_img}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <button className="view-resume-button">View Resume</button>
+                </a>
+            </div>
+
+
         </div>
+
     )
 }
 
